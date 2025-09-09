c3eaac7e26507152442119e6488fe4688d653384
Sergiu Tigan <sergiu@tigan.dev>
2025-09-09 07:13:59 +0300

    Fixed Edit Animal

--- src/common/dto/update-pet.dto.ts
+++ src/common/dto/update-pet.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsEmpty } from 'class-validator';
 import { CreatePetDto } from './create-pet.dto';
 
-export class UpdatePetDto extends PartialType(CreatePetDto) {}
+export class UpdatePetDto extends PartialType(CreatePetDto) {
+  @IsEmpty({ message: 'property id should not exist' })
+  id?: never;
+}
